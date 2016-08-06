d4691cfc9447eaf5ad9a35b304bc8c6a44b1e875
jayden <jxdjayden@gmail.com>
2016-08-07 00:46:02 +0800

    update

--- todo/js/index.js
+++ todo/js/index.js
@@ -2,9 +2,26 @@
  * @Author: jayden
  * @Date:   2016-08-06 21:32:39
  * @Last Modified by:   jayden
- * @Last Modified time: 2016-08-06 23:27:41
+ * @Last Modified time: 2016-08-07 00:22:29
  */
 
+
+(function() {
+ var list = {
+  all: function(todos) {
+   return todos;
+  },
+  active: function(todos) {
+   return todos.filter(function(todo) {
+    return !todo.completed;
+   });
+  },
+  completed: function(todos) {
+   return todos.filter(function(todo) {
+    return todo.completed;
+   });
+  }
+ };
 
  var vm = new Vue({
   el: '.todoapp',
@@ -12,6 +29,14 @@
    todos: [],
    newTodo: '',
    visit: 'all'
+  },
+  computed:{
+   filteredTodos: function () {
+    return list[this.visit](this.todos);
+   },
+   remaining: function () {
+    return list.active(this.todos).length;
+   }
   },
   methods: {
    addTodo: function() {
@@ -22,13 +47,17 @@
     } else {
      this.todos.push({
       value: newTodo,
-      completed:false
+      completed: false
      });
      this.newTodo = '';
     }
    },
-   removeTodo:function(todo){
+   removeTodo: function(todo) {
     this.todos.$remove(todo);
+   },
+   select: function(status) {
+    this.visit = status;
    }
   }
  });
+})();
